a574a4d90a5e212d91a5b56a6de3e24aa2f5632b
Shankholi Ray <shankandmail@gmail.com>
2023-02-14 16:43:16 +0530

    edits

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const fs=require("fs")
 const express=require('express');
 const  mongoose=require('mongoose');
 const cors=require("cors");
@@ -9,7 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // get post data
-
+app.get("/.well-known/pki-validation/86111B37272609CF88036152497F7944.txt", (req, res) => {
+let options = {
+      root: path.join(__dirname)
+    };
+    res.sendFile('86111B37272609CF88036152497F7944.txt',options,function (err) {
+      if (err) {
+          next(err);
+      } else {
+          console.log('Sent');
+      }
+  })
+  })
 app.use("/api", apiRouter);
 
 // const MONGODB_URI = "mongodb://127.0.0.1:27017/zomatoapi";
